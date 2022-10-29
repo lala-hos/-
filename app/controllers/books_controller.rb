dc7a3876b37b1eea7760774feeb331dfd672a672
EC2 Default User <ec2-user@ip-172-31-40-251.ap-northeast-1.compute.internal>
2022-10-29 09:38:52 +0000

    課題途中

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,25 @@
+class BooksController < ApplicationController
+  def create
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def book_params
+
+  end
+
+end
